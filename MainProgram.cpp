77f326a9a49a188956f4e3235ed49522ed3d32c7
tomeq3321 <doliwa1994@wp.pl>
2016-12-03 21:03:30 +0100

    Dodanie okna aplikacji
    Problem z oknem - zawiesza sie.

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -8,8 +8,19 @@ int main()
 {
 
 
+RenderWindow OknoAplikacji;
+Event zdarzenie;
 
+OknoAplikacji.create(VideoMode(1024,768,32),"Eat to Life - EOT");
 
+                                    while( OknoAplikacji.isOpen() )
+                            {
+
+                                while( OknoAplikacji.pollEvent( zdarzenie ) )
+                                {
+
+                                }
+                            }
 
     return 0;
 }
