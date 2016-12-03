80840951f4ffa8148880569ae4414325b28b5ee8
tomeq3321 <doliwa1994@wp.pl>
2016-12-03 21:12:50 +0100

    Naprawione główne okno Aplikacji
    Okno zostało naprawione, gotowe do dalszego wypełniania

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -8,19 +8,34 @@ int main()
 {
 
 
-RenderWindow OknoAplikacji;
-Event zdarzenie;
+RenderWindow OknoAplikacji; //obiekt który przetrzymuje okno aplikacji
+Event zdarzenie; // zdarzenia w programie
 
-OknoAplikacji.create(VideoMode(1024,768,32),"Eat to Life - EOT");
+//okno aplikacji/////////////////////////////////////////////////////////////////////////////////////
+        OknoAplikacji.create(VideoMode(1024,768,32),"Eat to Life - EOT"); // okno aplikacji
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-                                    while( OknoAplikacji.isOpen() )
+//Petla glowna gry///////////////////////////////////////////////////////////////////////////////////////
+                while( OknoAplikacji.isOpen() )
+                {
+
+                        while( OknoAplikacji.pollEvent( zdarzenie ) )
+                        {
+                            if( zdarzenie.type == sf::Event::KeyPressed && zdarzenie.key.code == sf::Keyboard::Escape )
                             {
-
-                                while( OknoAplikacji.pollEvent( zdarzenie ) )
-                                {
-
-                                }
+                                OknoAplikacji.close();
                             }
+
+                            if( zdarzenie.type == Event::Closed )
+                            {
+                                OknoAplikacji.close();
+                            }
+                        }
+
+                            OknoAplikacji.clear();
+                            OknoAplikacji.display();
+                            }
+/////////////////////////////////////////////////////////////////////////////////////////////
 
     return 0;
 }
